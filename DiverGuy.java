f35975838dea0ac6f1fb5d587178e62f108f0458
Hurr1cane- <92745357+Hurr1@users.noreply.github.com>
2022-02-11 20:04:45 +0300

    Update DiverGuy.java

--- DiverGuy.java
+++ DiverGuy.java
@@ -1,59 +1,67 @@
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-public class DiverGuy {
-    public int posX;
-    public int posY;
-    public int countOfGold;
-    boolean lifeStatus = true;
+public class DiverGuy
+{
+    private int pos_X;
+    private int pos_Y;
+    private int countOfGold;
+    private boolean lifeStatus = true;
 
     ImageView runningGuy;
 
-    public DiverGuy(int posX,int posY) throws FileNotFoundException {
-        this.posX = posX;
-        this.posY = posY;
+    public DiverGuy(int posX,int posY) throws FileNotFoundException
+    {
+        this.pos_X = posX;
+        this.pos_Y = posY;
         setCountOfGold(0);
         runningGuy = new ImageView(new Image(new FileInputStream("urPath/GuyOnBoard.png")));
     }
 
-    public void setCountOfGold(int newGoldValue) {
+    public void setCountOfGold(int newGoldValue)
+    {
         this.countOfGold = newGoldValue;
     }
 
-    public ImageView getGuyImage() {
+    public ImageView getGuyImage()
+    {
         return runningGuy;
     }
 
-    public int getPosX() {
-        return posX;
+    public int getPosX()
+    {
+        return pos_X;
     }
 
-    public int getPosY() {
-        return posY;
+    public int getPosY()
+    {
+        return pos_Y;
     }
 
-    public void it(){
+    public void it()
+    {
         setCountOfGold(getCountOfGold() + 1);
     }
 
-    public void toEmpty(){
+    public void toEmpty()
+    {
         setCountOfGold(0);
     }
 
-    public void kill(){
+    public void kill()
+    {
         lifeStatus = false;
     }
 
-    public boolean isAlive(){
+    public boolean isAlive()
+    {
         return lifeStatus;
     }
 
-    public int getCountOfGold() {
+    public int getCountOfGold()
+    {
         return countOfGold;
     }
 }
-
-
