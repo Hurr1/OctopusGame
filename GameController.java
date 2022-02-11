73355ebbc7eff5e3340546c74a15141b6d108bbf
Hurr1cane- <92745357+Hurr1@users.noreply.github.com>
2022-02-11 20:06:05 +0300

    Update GameController.java

--- GameController.java
+++ GameController.java
@@ -1,4 +1,3 @@
-package com.example.demo2;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -6,38 +5,24 @@ import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.event.EventHandler;
-
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-
-
 import java.util.HashMap;
+import java.util.Map;
 
-public class GameController {
+public class GameController
+{
 
-    HashMap<Integer, int[]> pos;
-    Scene scene;
-    Group root;
-    Storage storage;
-    DiverGuy diverGuy;
+    private final Map<Integer, int[]> POSITIONS;
+
     public GameController(Scene scene, Group root, Label textLabel,DiverGuy diverGuy, Storage storage) throws FileNotFoundException, InterruptedException {
 
-
-        pos = new HashMap<>();
-        pos.put(0, new int[]{195, 85});
-        pos.put(1, new int[]{185, 150});
-        pos.put(2, new int[]{220, 200});
-        pos.put(3, new int[]{290, 225});
-        pos.put(4, new int[]{370, 225});
-
-        this.scene = scene;
-        this.root = root;
-        this.storage = storage;
-        this.diverGuy = diverGuy;
-
-
-
+        POSITIONS = new HashMap<>();
+        POSITIONS.put(0, new int[]{195, 85});
+        POSITIONS.put(1, new int[]{185, 150});
+        POSITIONS.put(2, new int[]{220, 200});
+        POSITIONS.put(3, new int[]{290, 225});
+        POSITIONS.put(4, new int[]{370, 225});
 
         scene.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<>() {
             int positionIndex = 0;
@@ -50,15 +35,15 @@ public class GameController {
                         if (positionIndex >= 0) {
                             if (positionIndex > 0) {
                                 positionIndex--;
-                                diverGuy.getGuyImage().setX(pos.get(positionIndex)[0]);
-                                diverGuy.getGuyImage().setY(pos.get(positionIndex)[1]);
+                                diverGuy.getGuyImage().setX(POSITIONS.get(positionIndex)[0]);
+                                diverGuy.getGuyImage().setY(POSITIONS.get(positionIndex)[1]);
                             } else {
                                 storage.refill(diverGuy.getCountOfGold());
                                 textLabel.setText(storage.getGold() + "");
                                 diverGuy.setCountOfGold(0);
                                 positionIndex++;
-                                diverGuy.getGuyImage().setX(pos.get(positionIndex)[0]);
-                                diverGuy.getGuyImage().setY(pos.get(positionIndex)[1]);
+                                diverGuy.getGuyImage().setX(POSITIONS.get(positionIndex)[0]);
+                                diverGuy.getGuyImage().setY(POSITIONS.get(positionIndex)[1]);
                             }
                         }
 
@@ -67,14 +52,14 @@ public class GameController {
                         if (positionIndex < 5) {
                             positionIndex++;
                             if (positionIndex == 5) {
-                                if (diverGuy.countOfGold == 0) {
+                                if (diverGuy.getCountOfGold() == 0) {
                                     diverGuy.setCountOfGold(2);
                                 }
                                 diverGuy.it();
                                 positionIndex--;
                             }
-                            diverGuy.getGuyImage().setX(pos.get(positionIndex)[0]);
-                            diverGuy.getGuyImage().setY(pos.get(positionIndex)[1]);
+                            diverGuy.getGuyImage().setX(POSITIONS.get(positionIndex)[0]);
+                            diverGuy.getGuyImage().setY(POSITIONS.get(positionIndex)[1]);
                         }
                     }
                     if (positionIndex == 0) {
